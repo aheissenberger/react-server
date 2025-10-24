3384c13fbf32f949b9e1aded3dc429dd3ac059de
Andreas Heissenberger <andreas@heissenberger.at>
2025-10-24 23:11:07 +0200

    fix: restructure excludes in router config for clarity

--- examples/file-router/react-server.config.mjs
+++ examples/file-router/react-server.config.mjs
@@ -2,9 +2,14 @@ export default {
   root: "pages",
   router(config) {
     return {
+      // include existing default router config
       ...config,
-      // Exclude all files and directories within the "components" folder
-      excludes: ["components/**/*"],
+      excludes: [
+        // Important: existing excludes
+        ...config.excludes,
+        // Exclude all files and directories within the "components" folder
+        "components/**/*",
+      ],
     };
   },
 };
